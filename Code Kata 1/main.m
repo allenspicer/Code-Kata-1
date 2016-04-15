bbe2214fac5038679378fd21b098e3676d08f537
Allen Spicer <allenspicer@gmail.com>
2016-04-15 09:37:20 -0400

    Initial Commit

---
+++ Code Kata 1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Code Kata 1
+//
+//  Created by Allen Spicer on 4/15/16.
+//  Copyright © 2016 Allen Spicer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
