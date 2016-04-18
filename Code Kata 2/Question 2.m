cbc74197c62c620661e1a2b5a1b7c666a125146c
Allen Spicer <allenspicer@gmail.com>
2016-04-17 22:09:33 -0400

    Updated Answer
    Searches for Palindrome.
    Creates array of Palindromes which are the products of 2 3-digit
    integers.
    Removes largest palindrome and prints.
    Only one 3-digit number is dynamic, secondary is static.
    Wrapping second “while loop” never produced proper looping.

--- Code Kata 2/Question 2.m
+++ Code Kata 2/Question 2.m
@@ -10,72 +10,73 @@
 #import <malloc/malloc.h>
 
 int number;
-int primary =999;
-int secondary =999;
-
+int primary =1000;
+int secondary =995;
+int solution;
 
 int main(int argc, const char * argv[])
 {
-    @autoreleasepool
-    {
+        @autoreleasepool
+        {
         //create a new array to store palindromes
         NSMutableArray * arrayOfpalindromes = [NSMutableArray arrayWithObjects:NULL, nil];
-        
-        //create loop with primary value counting down until it equals 100
+
         while (primary >= 100)
         {
-        //create sub loop with secondary value counting down until it equals 100 and string being saved to array
-            while
-                (secondary >= 100)
-            {
-            //assign name to product of the two factors
-            number = primary * secondary;
-            // number must read forward the same as it reads backward
-            //take integer and save as string
-            NSString *numberName =[NSString stringWithFormat:@"%d", number];
-            //take last character from string and append it to a new, reversed string
-            //create a new mutable string varaible
-            NSMutableString *reversedString = [NSMutableString string];
-            //create a varable to identify the location of each character in the string
-            NSInteger charIndex = [numberName length];
+        //assign a name to the product of the two factors
+        number = primary * secondary;
+            
+        //decrement primary
+        primary --;
+                
+
+                // number must read forward the same as it reads backward
+                //take integer and save as string
+                NSString *numberName =[NSString stringWithFormat:@"%d", number];
+                
+                //take last character from string and append it to a new, reversed string
+                //create a new mutable string varaible
+                NSMutableString *reversedString = [NSMutableString string];
+            
+                //create a varable to identify the location of each character in the string
+                NSInteger charIndex = [numberName length];
+                
                 //create a while loop to cycle through the entire string
                 while (charIndex > 0)
                 {
-                // decrement variable by one each loop
-                charIndex--;
-                // create range to describe character position
-                NSRange subStrRange = NSMakeRange(charIndex, 1);
-                // append new string with each character in reverse order
-                [reversedString appendString:[numberName substringWithRange:subStrRange]];
+
+                        // decrement index variable
+                        charIndex--;
+                    
+                        // create range to describe character position
+                        NSRange subStrRange = NSMakeRange(charIndex, 1);
+                        
+                        // append new string with each character in reverse order
+                        [reversedString appendString:[numberName substringWithRange:subStrRange]];
+                        }
+                
+                        //compare reversed text to original text
+                        // if ([reversedString isEqualToString: numberName])
+                        if ([reversedString isEqualToString:numberName])
+                        {
+                        
+                        //add string to list of palindromes
+                        [arrayOfpalindromes addObject:numberName];
+                        
+                        //display results
+                        //NSLog(@"%@", numberName);
+                        //NSLog(@"%@", reversedString);
+                        //NSLog(@"%@", arrayOfpalindromes);
+                        }
                 }
-                //compare reversed text to original text
-               // if ([reversedString isEqualToString: numberName])
-                if ([reversedString isEqualToString:numberName])
-                {
-                //add string to list of palindromes
-                [arrayOfpalindromes addObject:numberName];
-                //display results
-                NSLog(@"%@", numberName);
-                NSLog(@"%@", reversedString);
-                NSLog(@"%@", arrayOfpalindromes);
-                }
-        //decrement primary
-                primary -= 1;
-            }
-            //reset primary
-            primary = 999;
-            //decrement secondary
-            secondary -= 1;
-          }
+        //remove largest palindrome from array
+        NSNumber * largestPalindrome = [arrayOfpalindromes valueForKeyPath:@"@max.intValue"];
+        //print largest palindrome
+        NSLog(@"%@", largestPalindrome);
         
-    //decrement secondary while primary is on this number, then continue
-    
-        
-    //find and print largest palindrome in array
-
-
-    }
-
-
+        }
     return 0;
 }
+
+
+//find and print largest palindrome in array
